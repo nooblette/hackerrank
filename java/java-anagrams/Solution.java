0db190e87b0c4e3b0b7206c63214e24773d2813b
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-04-27 21:39:49 +0900

    Update Solution.java

--- java/java-anagrams/Solution.java
+++ java/java-anagrams/Solution.java
@@ -4,20 +4,16 @@ import java.util.*;
 
 public class Solution {
     static boolean isAnagram(String a, String b) {
+        if(a.length() != b.length()) {
+            return false;
+        }
         char[] aArray = a.toUpperCase().toCharArray();
         char[] bArray = b.toUpperCase().toCharArray();
         
         Arrays.sort(aArray);
         Arrays.sort(bArray);
         
-        int size = aArray.length < bArray.length ? aArray.length : bArray.length;
-        for(int i = 0; i < size; i++) {
-            if(aArray[i] != bArray[i]) {
-                return false;
-            }
-        }
-        
-        return aArray.length == bArray.length;
+        return Arrays.equals(aArray, bArray);
     }
 
     public static void main(String[] args) {
