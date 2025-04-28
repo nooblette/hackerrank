f820973eebd42e6c91aa4397a279e3b269ac625b
Oh Min Hyeok <69203354+nooblette@users.noreply.github.com>
2025-04-28 20:48:25 +0900

    Java String Tokens

---
+++ java/java-string-tokens/Solution.java
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/java-string-tokens/problem
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine().trim();
+        scanner.close();
+        
+        String[] tokens = s.split("[ !,?._'@]+");
+        if(s.isEmpty()) {
+            System.out.println(0);
+        } else {
+            System.out.println(tokens.length);
+            for(String token : tokens) {
+                System.out.println(token);
+            }
+        }
+    }
+}
